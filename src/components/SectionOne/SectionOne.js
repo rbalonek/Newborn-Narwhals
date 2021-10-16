8430b2845c5be430f02dd5f231c053575541b15b
Robert Balonek <robertbalonek@gmail.com>
2021-10-16 09:05:51 -0700

    New Breakpoints Centered

--- src/components/SectionOne/SectionOne.js
+++ src/components/SectionOne/SectionOne.js
@@ -6,23 +6,23 @@ import { Fade, Slide, AttentionSeeker } from "react-awesome-reveal";
 export default function SectionOne() {
   return (
     <div className="sectionOne__container">
-      <AttentionSeeker triggerOnce={true} effect="tada" delay="200">
-        <Slide triggerOnce={true} direction="down" duration="2000">
-          <img
-            className="pic_one"
-            alt="picOne"
-            src="https://res.cloudinary.com/bobalobbadingdong/image/upload/v1634322415/Newborn-Narwhals/PIC_1_z4quhf.png"
-          />
-        </Slide>
-      </AttentionSeeker>
-      <div className="sectionOne__text">
-        <Fade duration={3000} triggerOnce="true">
-          <h1>Collectable</h1>
-          <h1 className="digital-art">Digital Art</h1>
-          <h4 style={{ position: "relative", bottom: "60px" }}>
-            NFTs with a fun side
-          </h4>
-        </Fade>
+      <div className="sectionOne__center-keeper">
+        <AttentionSeeker triggerOnce={true} effect="tada" delay="200">
+          <Slide triggerOnce={true} direction="down" duration="2000">
+            <img
+              className="pic_one"
+              alt="picOne"
+              src="https://res.cloudinary.com/bobalobbadingdong/image/upload/v1634322415/Newborn-Narwhals/PIC_1_z4quhf.png"
+            />
+          </Slide>
+        </AttentionSeeker>
+        <div className="sectionOne__text">
+          <Fade duration={3000} triggerOnce="true">
+            <h1 className="sectionOne-collectable">Collectable</h1>
+            <h1 className="digital-art">Digital Art</h1>
+            <p className="sectionOne__text-nfts">NFTs with a fun side</p>
+          </Fade>
+        </div>
       </div>
     </div>
   );
--- src/components/SectionTwo/SectionTwo.js
+++ src/components/SectionTwo/SectionTwo.js
@@ -6,25 +6,27 @@ import { Fade, Slide } from "react-awesome-reveal";
 export default function SectionTwo() {
   return (
     <div className="SectionTwo__container">
-      <div className="sectionTwo__text">
-        <Fade duration={3000} triggerOnce="true">
-          <h4 style={{ lineHeight: "30px" }}>
-            Rising from the deep blue sea...our baby narwhals are here to steal
-            your heart Each narwhal is randomly generated and 100% unique. Are
-            you ready to play our game and adopt an endangered NFT into your
-            heart?
-          </h4>
-        </Fade>
+      <div className="SectionTwo__center-keeper">
+        <div className="sectionTwo__text">
+          <Fade duration={3000} triggerOnce="true">
+            <h4 style={{ lineHeight: "30px" }}>
+              Rising from the deep blue sea...our baby narwhals are here to
+              steal your heart Each narwhal is randomly generated and 100%
+              unique. Are you ready to play our game and adopt an endangered NFT
+              into your heart?
+            </h4>
+          </Fade>
+        </div>
+        <Slide
+        // direction="up"
+        >
+          <img
+            className="pic_two"
+            alt="picTwo"
+            src="https://res.cloudinary.com/bobalobbadingdong/image/upload/v1634323211/Newborn-Narwhals/PIC_2_copy_opryxn.png"
+          />
+        </Slide>
       </div>
-      <Slide
-      // direction="up"
-      >
-        <img
-          className="pic_two"
-          alt="picTwo"
-          src="https://res.cloudinary.com/bobalobbadingdong/image/upload/v1634323211/Newborn-Narwhals/PIC_2_copy_opryxn.png"
-        />
-      </Slide>
     </div>
   );
 }
