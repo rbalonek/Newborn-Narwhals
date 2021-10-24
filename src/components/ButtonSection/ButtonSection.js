a20951f056c5895dcbf22a5a491967145cefdd28
Robert Balonek <robertbalonek@gmail.com>
2021-10-24 11:03:58 -0400

    Hook up Btns

--- src/components/ButtonSection/ButtonSection.js
+++ src/components/ButtonSection/ButtonSection.js
@@ -17,15 +17,26 @@ export default function ButtonSection() {
         </button>
 
         <div className="twit-dis__btns">
-          <button className="twitter-btn btn">
-            <img className="twitter-dtn-img" alt="img" src={TwitterImg} />
-            Twitter
-          </button>
-
-          <button className="discord-btn btn">
-            <img className="discord-dtn-img" alt="img" src={DiscordImg} />
-            Discord
-          </button>
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://twitter.com/newbornnarwhals"
+          >
+            <button className="twitter-btn btn">
+              <img className="twitter-dtn-img" alt="img" src={TwitterImg} />
+              Twitter
+            </button>
+          </a>
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://discord.com/invite/5dgtRRmSnh"
+          >
+            <button className="discord-btn btn">
+              <img className="discord-dtn-img" alt="img" src={DiscordImg} />
+              Discord
+            </button>
+          </a>
         </div>
       </div>
     </div>
